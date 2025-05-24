3b502536b746aadcd17dd939798a01a2f947c3f2
pushpendra singh <singh16195@gmail.com>
2025-05-24 13:11:59 +0530

    removed api key

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,12 +7,13 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { firebaseConfig } from './firebaseConfig';
 import { routes } from './app.routes';
 
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimationsAsync(),
     provideFirebaseApp(() => initializeApp(firebaseConfig)),
     provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore()), provideFirebaseApp(() => initializeApp({ projectId: "ng-form-builder-app", appId: "1:745855044535:web:44e41ebd40dfb329c53632", storageBucket: "ng-form-builder-app.firebasestorage.app", apiKey: "AIzaSyCPyc9KtUk83dFSyzJcTcSOYpoSjgJUBdE", authDomain: "ng-form-builder-app.firebaseapp.com", messagingSenderId: "745855044535" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())
+    provideFirestore(() => getFirestore()), provideFirebaseApp(() => initializeApp(firebaseConfig)), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())
   ]
 };
